45b5df39abbdadf8b21c2821d38c36e218a202b2
JavCho23 <javier120699lili@gmail.com>
2020-02-29 11:55:50 -0500

    feat: created fake get score

---
+++ src/client/products/meal/aplication/get_score/get_score.js
@@ -0,0 +1,11 @@
+const MealScore = require("../../domain/value/meal_score");
+class GetScore {
+  constructor(mealId) {
+    this._mealId = mealId;
+  }
+  call() {
+    return new MealScore(Math.random() * (5 + 0) + 0);
+  }
+}
+
+module.exports = GetScore;
