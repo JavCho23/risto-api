37ce453300d5e34f1010357490f40026c67e6e00
JavCho23 <javier120699lili@gmail.com>
2020-02-29 10:17:23 -0500

    feat: Create entity tag

---
+++ src/client/shared/domain/tag/value/tag_id.js
@@ -0,0 +1,9 @@
+const RawString = require("../../../../../shared/domain/value/raw_string");
+
+class TagId extends RawString {
+  constructor(value) {
+    super(value);
+  }
+}
+
+module.exports = TagId;
