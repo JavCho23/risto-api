bacf7b3b4029ef4b03107c2dabbe6ae572d5d574
JavCho23 <javier120699lili@gmail.com>
2020-02-29 10:24:48 -0500

    feat: Create value category name

---
+++ src/client/shared/domain/category/value/category_name.js
@@ -0,0 +1,9 @@
+const RawString = require("../../../../../shared/domain/value/raw_string");
+
+class CategoryName extends RawString {
+  constructor(value) {
+    super(value);
+  }
+}
+
+module.exports = CategoryName;
