6c77873a0a997c5c4edfee485cca4a85e43c80e8
JavCho23 <javier120699lili@gmail.com>
2020-03-06 23:33:55 -0500

    fix: Pase the value

--- src/client/products/meal/domain/value/meal_score.js
+++ src/client/products/meal/domain/value/meal_score.js
@@ -2,7 +2,7 @@ const RawDouble = require("../../../../../shared/domain/value/raw_double");
 
 class MealScore extends RawDouble {
   constructor(value) {
-    super(value.toFixed(2));
+    super(parseFloat(value.toFixed(1)));
   }
 }
 
