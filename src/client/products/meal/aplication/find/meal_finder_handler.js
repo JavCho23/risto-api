3fd37ac8e1e2d4e2223edf790dddd70f1bc08dc8
JavCho23 <javier120699lili@gmail.com>
2020-03-12 20:17:28 -0500

    dev: Formating code

--- src/client/products/meal/aplication/find/meal_finder_handler.js
+++ src/client/products/meal/aplication/find/meal_finder_handler.js
@@ -3,22 +3,22 @@ const MysqlMealRepository = require("../../infrastructure/mysql_meal_repository"
 const MealId = require("../../domain/value/meal_id");
 const State = require("../../../../../shared/domain/state");
 exports.mealFindHandler = async event => {
-  const { pathParameters } = event;
-  const response = {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    isBase64Encoded: false
-  };
-  try {
-    const mealfinder = new MealFinder(new MysqlMealRepository());
-    let result = await mealfinder.call(
-      new MealId(pathParameters.id),
-      new State(true)
-    );
-    response.body = JSON.stringify(result.toJson());
-  } catch (error) {
-    response.statusCode = 404;
-    response.body = JSON.stringify({ message: error.message });
-  }
-  return response;
+    const { pathParameters } = event;
+    const response = {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        isBase64Encoded: false
+    };
+    try {
+        const mealfinder = new MealFinder(new MysqlMealRepository());
+        let result = await mealfinder.call(
+            new MealId(pathParameters.id),
+            new State(true)
+        );
+        response.body = JSON.stringify(result.toJson());
+    } catch (error) {
+        response.statusCode = 404;
+        response.body = JSON.stringify({ message: error.message });
+    }
+    return response;
 };
--- src/client/products/meal/domain/value/meal_score.js
+++ src/client/products/meal/domain/value/meal_score.js
@@ -1,9 +1,9 @@
 const RawDouble = require("../../../../../shared/domain/value/raw_double");
 
 class MealScore extends RawDouble {
-  constructor(value) {
-    super(parseFloat(value.toFixed(1)));
-  }
+    constructor(value) {
+        super(parseFloat(value.toFixed(1)));
+    }
 }
 
 module.exports = MealScore;
