d50d7a5df558f26781495123c1c0767b96a95cd0
JavCho23 <javier120699lili@gmail.com>
2020-04-09 01:50:10 -0500

    fix: Add json.parse

--- src/auth/token/aplication/auth_login_handler.js
+++ src/auth/token/aplication/auth_login_handler.js
@@ -1,14 +1,13 @@
 const Login = require("./login");
 exports.authLoginHandler = async event => {
-    const {body} = event;
-    console.log(event);
+    const body = JSON.parse( event.body);
+    
     const response = {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         isBase64Encoded: false
     };
     try {
-        console.log(body);
         const login = new Login(body, "mySecurityKey");
         const token = await login.call();
         response.body = JSON.stringify({ token: token });
