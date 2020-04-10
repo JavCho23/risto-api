bc1ca154abc03f3b0bef0700952c440b6ba3f94d
JavCho23 <javier120699lili@gmail.com>
2020-04-10 16:04:48 -0500

    dev: Using queryStringParameters

--- src/manager/community/local/stats/aplication/stats_get_handler.js
+++ src/manager/community/local/stats/aplication/stats_get_handler.js
@@ -1,20 +1,22 @@
-const CommunityStatsGet = require("./stats_get");
+const CommunityStatsGet = require("./community_stats_get");
 const MySqlCommunityStatsRepository = require("../infrastructure/mysql_community_stats_repository");
 
-exports.CommunityStatsGetHandler = async event => {
-    const { pathParameters } = event;
+exports.statsGetHandler = async event => {
+    const { pathParameters , queryStringParameters } = event;
     const response = {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         isBase64Encoded: false
     };
     try {
+       if(queryStringParameters.type == community){
         const qualificationGet = new CommunityStatsGet(
             new MySqlCommunityStatsRepository()
         );
         const data = await qualificationGet.call(pathParameters.id);
         const result = data.toJson(); 
         response.body = JSON.stringify(result);
+       }
     } catch (error) {
         response.statusCode = 404;
         response.body = JSON.stringify({ message: error.message });
