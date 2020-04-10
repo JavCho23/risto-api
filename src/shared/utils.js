444a9c718ecd5a1845091a904a18dd38c37aee2b
JavCho23 <javier120699lili@gmail.com>
2020-04-10 12:01:00 -0500

    dev: Adding initial value

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,6 +1,6 @@
 
 
 exports.calculateRanting = (scores) => {
-    return scores.reduce((previous,current)=> previous.amount.value + current.amount.value) / scores.length;  
+    return scores.reduce((previous,current)=> previous.amount.value + current.amount.value,0) / scores.length;  
 };
 
