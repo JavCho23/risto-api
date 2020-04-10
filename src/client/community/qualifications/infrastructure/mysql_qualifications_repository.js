17d8f882f3c8e743fb09daa6ce138d747423e911
JavCho23 <javier120699lili@gmail.com>
2020-04-09 21:00:47 -0500

    dev: Refctoring

--- src/client/community/qualifications/infrastructure/mysql_qualifications_repository.js
+++ src/client/community/qualifications/infrastructure/mysql_qualifications_repository.js
@@ -1,16 +1,16 @@
-const QualificationList = require("../../qualification/aplication/qualification_list");
-const MySqlQualificationRepository = require("../../qualification/infrastructure/mysql_qualification_repository");
-const SummaryGet = require("../../summary/aplication/qualifications_list");
+const CommentList = require("../../comment/aplication/comment_list");
+const MySqlCommentRepository = require("../../comment/infrastructure/mysql_comment_repository");
+const SummaryGet = require("../../summary/aplication/summary_get");
 const MySqlSummaryRepository = require("../../summary/infrastructure/mysql_summary_repository");
 const Qualifications = require("../domain/qualifications");
 class MySqlQualificationsRepository {
 
     async list(mealId) {
 
-        const qualificationList = new QualificationList(
-            new MySqlQualificationRepository()
+        const commentList = new CommentList(
+            new MySqlCommentRepository()
         );
-        const comments   = await qualificationList.call(mealId);
+        const comments   = await commentList.call(mealId);
         
         const summaryGet = new SummaryGet(new MySqlSummaryRepository());
         const summary    = await summaryGet.call(mealId,comments);
