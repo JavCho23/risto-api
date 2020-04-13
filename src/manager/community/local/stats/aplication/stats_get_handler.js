510d89e89032c472ba01a811928bdbf15ff7401e
JavCho23 <javier120699lili@gmail.com>
2020-04-13 18:31:50 -0500

    dev: Filtering starts type

--- src/manager/community/local/stats/aplication/stats_get_handler.js
+++ src/manager/community/local/stats/aplication/stats_get_handler.js
@@ -1,4 +1,3 @@
-const CommunityStatsGet = require("./community_stats_get");
 const MySqlCommunityStatsRepository = require("../infrastructure/mysql_community_stats_repository");
 
 exports.statsGetHandler = async event => {
@@ -9,14 +8,13 @@ exports.statsGetHandler = async event => {
         isBase64Encoded: false
     };
     try {
-       if(queryStringParameters.type == "community"){
-        const qualificationGet = new CommunityStatsGet(
-            new MySqlCommunityStatsRepository()
-        );
+        const {type} = queryStringParameters ;
+        const StatsGet = require("./"+type+"_stats_get");
+        const qualificationGet = new StatsGet(new MySqlCommunityStatsRepository())
         const data = await qualificationGet.call(pathParameters.id);
         const result = data.toJson(); 
         response.body = JSON.stringify(result);
-       }
+       
     } catch (error) {
         response.statusCode = 404;
         response.body = JSON.stringify({ message: error.message });
