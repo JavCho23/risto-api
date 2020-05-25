8b56b3b7bdab1395c54b4439243554602ac1f77c
JavCho23 <javier120699lili@gmail.com>
2020-05-24 21:13:08 -0500

    dev: Adding parse to double

--- src/shared/domain/value/raw_double.js
+++ src/shared/domain/value/raw_double.js
@@ -1,14 +1,14 @@
 class RawNumber {
-    constructor(value) {
-        this._value = value;
-    }
+  constructor(value) {
+    this._value = value;
+  }
 
-    get value() {
-        return this._value;
-    }
-    toString() {
-        this._value.toString();
-    }
+  get value() {
+    return parseFloat(this._value);
+  }
+  toString() {
+    this._value.toString();
+  }
 }
 
 module.exports = RawNumber;
