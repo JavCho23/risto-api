08a4401403070db7e46ec2f5c1d356474d3c2143
JavCho23 <javier120699lili@gmail.com>
2020-05-24 23:25:52 -0500

    dev: Add idUser

--- src/delivery/profile/handler/profile_add.js
+++ src/delivery/profile/handler/profile_add.js
@@ -1,19 +1,21 @@
 const MySqlProfileRepository = require("../infrastructure/mysql_profile_repository");
 const ProfileAdder = require("../aplication/add/profile_adder");
 const MySqlLocationRepository = require("../../location/infrastructure/mysql_location_repository");
-const LocationAdder = require("../../location/aplication/");
+const LocationAdder = require("../../location/aplication/add/location_adder");
+const JWT = require("jsonwebtoken");
 
 const NoContentReponse = require("../../../shared/domain/response/no_content_response");
 const ErrorResponse = require("../../../shared/domain/response/error_response");
 
 exports.addProfile = async (event) => {
-  const { pathParameters } = event;
+  const { headers } = event;
   const bodyRequest = JSON.parse(event.body);
   let response;
   try {
     const profileAdder = new ProfileAdder(new MySqlProfileRepository());
     await profileAdder.call(
-      { ...bodyRequest, ...{ idProfile: pathParameters.id } },
+      new Uuid(JWT.decode(headers["x-api-key"]).idUser),
+      bodyRequest ,
       new LocationAdder(new MySqlLocationRepository())
     );
     response = new NoContentReponse();
