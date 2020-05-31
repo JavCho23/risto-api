c2e4d85985acd6eaab08010827c41aa782103095
JavCho23 <javier120699lili@gmail.com>
2020-05-31 16:18:21 -0500

    dev: Fromating files

--- src/delivery/item/aplication/add/item_adder_from_excel.js
+++ src/delivery/item/aplication/add/item_adder_from_excel.js
@@ -1,12 +1,12 @@
 const Item = require("../../domain/item");
 
 class ItemAdderFromExcel {
-  constructor(repository) {
-    this.repository = repository;
-  }
-  call(idLocal, base64String, productAdder,recordAdder) {
-    return this.repository.addFromExcel(idLocal, base64String, productAdder,recordAdder);
-  }
+    constructor(repository) {
+        this.repository = repository;
+    }
+    call(idLocal, base64String, productAdder,recordAdder) {
+        return this.repository.addFromExcel(idLocal, base64String, productAdder,recordAdder);
+    }
 }
 
 module.exports = ItemAdderFromExcel;
