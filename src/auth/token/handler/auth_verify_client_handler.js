f23fc8d45c638686730987ea1070a43376536bf0
JavCho23 <javier120699lili@gmail.com>
2020-05-31 16:05:22 -0500

    dev: Using function from utils

--- src/auth/token/handler/auth_verify_client_handler.js
+++ src/auth/token/handler/auth_verify_client_handler.js
@@ -1,15 +1,16 @@
 const VerifyToken = require("../aplication/verify_client_token");
 const keys = require('../infrastructure/persitence/securitykey.json')
+const Utils =require('../../../shared/domain/utils.js')
 
 exports.authVerifyHandler = function (event, context, callback) {
     const token = event.authorizationToken;
     const verifyToken = new VerifyToken(token);
     switch (verifyToken.call(keys.dev)) {
     case "allow":
-        callback(null, generatePolicy("client", "Allow", event.methodArn));
+        callback(null, Utils.generatePolicy("client", "Allow", event.methodArn));
         break;
     case "deny":
-        callback(null, generatePolicy("client", "Deny", event.methodArn));
+        callback(null, Utils.generatePolicy("client", "Deny", event.methodArn));
         break;
     case "unauthorized":
         callback("Unauthorized");   // Return a 401 Unauthorized response
@@ -17,23 +18,4 @@ exports.authVerifyHandler = function (event, context, callback) {
     default:
         callback("Error: Invalid token"); // Return a 500 Invalid token response
     }
-};
-
-// Help function to generate an IAM policy
-var generatePolicy = function (principalId, effect, resource) {
-    var authResponse = {};
-
-    authResponse.principalId = principalId;
-    if (effect && resource) {
-        var policyDocument = {};
-        policyDocument.Version = "2012-10-17";
-        policyDocument.Statement = [];
-        var statementOne = {};
-        statementOne.Action = "execute-api:Invoke";
-        statementOne.Effect = effect;
-        statementOne.Resource = resource;
-        policyDocument.Statement[0] = statementOne;
-        authResponse.policyDocument = policyDocument;
-    }
-    return authResponse;
 };
