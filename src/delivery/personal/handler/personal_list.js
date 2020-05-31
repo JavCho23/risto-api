beb54639f048a604e259beda9b3e6af68679813a
JavCho23 <javier120699lili@gmail.com>
2020-05-30 23:52:27 -0500

    feat: Add handlers from personal

---
+++ src/delivery/personal/handler/personal_list.js
@@ -0,0 +1,18 @@
+const MySqlPersonalRepository = require("../infrastructure/mysql_personal_repository");
+const PersonalLister = require("../aplication/list/personal_lister");
+const Uuid = require("../../../shared/domain/value/uuid");
+const SuccessResponde = require("../../../shared/domain/response/success_response");
+const ErrorResponse = require("../../../shared/domain/response/error_response");
+
+exports.listPersonal = async (event) => {
+  const { pathParameters } = event;
+  let response;
+  try {
+    const personalLister = new PersonalLister(new MySqlPersonalRepository());
+    const body = await personalLister.call(new Uuid(pathParameters.id));
+    response = new SuccessResponde(body.map((manager) => manager.toJson()));
+  } catch (error) {
+    response = new ErrorResponse(error);
+  }
+  return response.toJson();
+};
---
+++ src/delivery/personal/handler/personal_remove.js
@@ -0,0 +1,18 @@
+const MySqlPersonalRepository = require("../infrastructure/mysql_personal_repository");
+const PersonalRemover = require("../aplication/remove/personal_remover");
+const Uuid = require("../../../shared/domain/value/uuid");
+const NoContentResponse = require("../../../shared/domain/response/no_content_response");
+const ErrorResponse = require("../../../shared/domain/response/error_response");
+
+exports.removePersonal = async (event) => {
+  const { pathParameters } = event;
+  let response;
+  try {
+    const personalRemover = new PersonalRemover(new MySqlPersonalRepository());
+    await personalRemover.call(new Uuid(pathParameters.idPersonal));
+    response = new NoContentResponse();
+  } catch (error) {
+    response = new ErrorResponse(error);
+  }
+  return response.toJson();
+};
