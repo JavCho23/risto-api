1b6508509389d88af16478bfc88aab5d71ed12e7
JavCho23 <javier120699lili@gmail.com>
2020-06-15 21:28:52 -0500

    feat: Create shuffle function

--- src/shared/domain/utils.js
+++ src/shared/domain/utils.js
@@ -1,49 +1,68 @@
 const XLSX = require("xlsx");
 
 function paginate(array, limit, offset) {
-    if(!(limit | offset)) return array;
-    limit+=offset;
-    return array.filter((x, index) => index >= offset && index < limit);
+  if (!(limit | offset)) return array;
+  limit += offset;
+  return array.filter((x, index) => index >= offset && index < limit);
 }
 function sheet2array(sheet) {
-    var result = [];
-    var row;
-    var rowNum;
-    var colNum;
-    var range = XLSX.utils.decode_range(sheet["!ref"]);
-    for (rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
-        row = [];
-        for (colNum = range.s.c; colNum <= range.e.c; colNum++) {
-            var nextCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: colNum })];
-            if (typeof nextCell === "undefined") {
-                row.push(void 0);
-            } else row.push(nextCell.w);
-        }
-        result.push(row);
+  var result = [];
+  var row;
+  var rowNum;
+  var colNum;
+  var range = XLSX.utils.decode_range(sheet["!ref"]);
+  for (rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
+    row = [];
+    for (colNum = range.s.c; colNum <= range.e.c; colNum++) {
+      var nextCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: colNum })];
+      if (typeof nextCell === "undefined") {
+        row.push(void 0);
+      } else row.push(nextCell.w);
     }
-    return result;
+    result.push(row);
+  }
+  return result;
 }
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-var generatePolicy = function(principalId, effect, resource) {
-    var authResponse = {};
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-    authResponse.principalId = principalId;
-    if (effect && resource) {
-        var policyDocument = {};
-        policyDocument.Version = "2012-10-17";
-        policyDocument.Statement = [];
-        var statementOne = {};
-        statementOne.Action = "execute-api:Invoke";
-        statementOne.Effect = effect;
-        statementOne.Resource = resource;
-        policyDocument.Statement[0] = statementOne;
-        authResponse.policyDocument = policyDocument;
-    }
-    return authResponse;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+var generatePolicy = function (principalId, effect, resource) {
+  var authResponse = {};
+
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = "2012-10-17";
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = "execute-api:Invoke";
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
 };
 
 module.exports = {
-    paginate: paginate,
-    sheetToArray: sheet2array,
-    generatePolicy: generatePolicy
+  paginate: paginate,
+  sheetToArray: sheet2array,
+  generatePolicy: generatePolicy,
+  shuffle: shuffle,
 };
